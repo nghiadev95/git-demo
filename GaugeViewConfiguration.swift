2b174d040e815bf220a32b1ade71300e93a31c9a
Nghia Nguyen <nghianguyen@teqnological.asia>
2018-12-19 16:13:58 +0700

    2

--- GaugeViewConfiguration.swift
+++ GaugeViewConfiguration.swift
@@ -14,7 +14,7 @@ class GaugeViewCon2222figuration111 {
         case atIntersection
         case afterIntersection
     }
-    1
+    12
     let animationDuration: Double
     let radianUnit: CGFloat
     let rightUnitPadding: NSNumber
