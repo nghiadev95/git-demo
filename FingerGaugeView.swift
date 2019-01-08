f620696791c4a882b0e42335a3a461c188b7a190
Nghia Nguyen <nghianguyen@teqnological.asia>
2019-01-08 14:45:31 +0700

    hotfix2

--- FingerGaugeView.swift
+++ FingerGaugeView.swift
@@ -12,7 +12,7 @@ class FingerGaugeView: UIView {
     
     let shapLayer = CAShapeLayer()
     
-    override func awakeh1FromNib() {
+    override func awakeh2FromNib() {
         super.awakeFromNib()
         shapLayer.fillColor = UIColor.primarygrey.cgColor
         shapLayer.lineCap = CAShapeLayerLineCap.round
@@ -21,7 +21,7 @@ class FingerGaugeView: UIView {
         layer.addSublayer(shapLayer)
     }
     
-    override func layouth1Subviews() {
+    override func layouth2Subviews() {
         super.layoutSubviews()
         let bezierPath = UIBezierPath()
         bezierPath.move(to: CGPoint(x: 0, y: bounds.h * 0.2))
